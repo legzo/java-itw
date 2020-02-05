a60c4ce2348de0c027dc9d5aef7404ff322bbd96
Julien Terrier <legzomil@gmail.com>
2020-02-05 20:09:55 +0100

    Update des versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
 }
 
 group = "com.orange.ccmd.dojo"
@@ -13,10 +13,10 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.slf4j:slf4j-api:1.7.28")
+    implementation("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     testImplementation("io.kotlintest:kotlintest-assertions:3.4.2")
 }
 
