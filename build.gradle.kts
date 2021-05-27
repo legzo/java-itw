db767ddd1e1c94167a4a252c4cef3747a728a6a1
Julien Terrier <julien.terrier@orange.com>
2021-05-27 10:35:10 +0200

    Met à jour les dépendances

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.10"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 group = "com.orange.ccmd.dojo"
@@ -16,7 +17,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
     testImplementation("io.kotest:kotest-assertions-jvm:4.0.7")
 }
 
@@ -26,4 +27,22 @@ tasks.withType<Test> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.dependencyUpdates {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "b", "rc", "cr", "m", "preview", "eap", "M")
+                    .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                    .any { it.matches(candidate.version) }
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "$projectDir/build/dependencyUpdates"
 }
