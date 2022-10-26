d03cf934b94a0bf256ca34ef0569672ae4e36191
Julien Terrier <julien.terrier@orange.com>
2022-10-26 10:53:08 +0200

    Déplace le TU

--- src/test/java/com/orange/ccmd/itw/DemoTest.java
+++ src/test/java/com/orange/ccmd/itw/DemoTest.java
@@ -1,3 +1,6 @@
+package com.orange.ccmd.itw;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,6 +9,6 @@ class DemoTest {
 
     @Test
     void shouldAddNumbers() {
-        assertEquals(4, 2 + 2);
+        Assertions.assertEquals(4, 2 + 2);
     }
 }
